23a306c11ad130c219757005a2a3128f8bbae4c3
ColbyTief <78509355+ColbyTief@users.noreply.github.com>
2021-04-22 20:46:36 -0500

    Updated Landing Page

--- README.md
+++
@@ -1,2 +0,0 @@
-## 2021_DeskDash
--Newest Iteration at https://github.com/ColbyTief/2021_DeskDash
---
+++ js/shopping_cart.js
@@ -0,0 +1,26 @@
+//$(function(){
+//  //Initialize variables of the elements that are being animated. 
+//  var $shoppingCart = $('#shoppingCart');
+//  var $loginButton = $('#login');
+//
+//  $shoppingCart.css({
+//    opacity: '20%',
+//    transform: 'scale(' + 10 + ',' + 20 + ')'
+//  }).animate({
+//    transform: 'scale(' + 1 + ')',
+//    opacity: '80%'
+//  }, 1000, 'easeInQuad', function() {
+//    $shoppingCart.animate({ 
+//      left: '-205px'
+//    }, 500, 'easeInElastic')
+//  })
+//});
+
+$(function() {
+  var $shoppingCart = $('#shoppingCart');
+  $shoppingCart.animate({left: "-=500"}, 500);
+  $shoppingCart.animate({left: "+=500"}, 1000);
+  $shoppingCart.animate({
+    opacity: '70%'
+  }, 1000)
+});
