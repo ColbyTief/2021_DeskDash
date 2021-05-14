7b15ae5f89cc8e15e6c0209873da53bfe023222a
ColbyTief <78509355+ColbyTief@users.noreply.github.com>
2021-05-13 20:02:50 -0500

    Update to order page
    also small changes to other pages

--- order.php
+++ order.php
@@ -17,27 +17,49 @@
 
 	<?php
 	if(isset($_GET['type'])) {
+		
 		// echo '<h2> HELLO! </h2>';
 		// SQL select by food type here 
 		$type = sanitizeString(INPUT_GET, 'type');
-		$query = "SELECT fooditem FROM fooditems
+		$query = "SELECT fooditem, imgpath, cost FROM fooditems
 				  WHERE foodtype = $type;";
 		$error = "Error fetching food info";
 
 		$foodResult = callQuery($pdo, $query, $error);
 
-
+?> 
+	<h3>Add Item to checkout</h3>
+	<ul class="foodList">
+<?php 
 		// output all returned food items from query
 		while ($item = $foodResult->fetch()) {
-			echo '<h2>';
-			echo $item['fooditem'];
-			echo '</h2>';
-		}		
+
+			$imgPath = $item['imgpath'];
+			$foodItem = $item['fooditem'];
+			$cost = $item['cost'];
+			if (isset($cost)) { 
+				$costStr = "$" . number_format($cost, 2);
+			}else{ 
+				$costStr = "Cost Here";
+			}
+
+			?>
+
+		<li><img src="/images/food/<?php if (!$imgPath == "" || isset($imgPath)) { echo "$imgPath";}else{ echo "default.png";} ?>"><p class="foodItem"><?= $foodItem ?></p> 
+		<p class="cost"><?= $costStr ?></p></li>
+
+		<?php
+
+		}
+		
+		?> 
+		</ul>
+	<?php 
 
 	} else {
 	?>
 		<h3>Choose a type</h3>
-		<ul id="typelist">
+		<ul class="typeList">
 		<a href="order.php?type=1"><li>American</li></a>
 		<a href="order.php?type=2"><li>Chinese</li></a>
 		<a href="order.php?type=3"><li>Mexican</li></a>
