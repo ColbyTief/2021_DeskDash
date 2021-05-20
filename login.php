81e95bcdd2518689de633a5c2d87856e3f56b257
cvtc-Dylan-McNew <78509880+cvtc-Dylan-McNew@users.noreply.github.com>
2021-05-20 13:22:01 -0500

    [(Delivers|Finishes) #178208609], [(Delivers|Finishes) #178208598],  [(Delivers|Finishes) #178205607], [(Delivers|finishes) #178205597], [(Delivers|finishes) #178205585], [(Delivers|finishes) #178205565]
    #178208609 polite greeting mesage for repeat customers.
    #178208598 Usernames are unique when signing up.
    #178205607 Users are stored in a database so they can come back and log into the site.
    #178205597 Greets a returning user or a new user based on weather they previously signed up or not.
    #178205585 Returning users still have accounts when leaving the site because of db connection and credential checking for example a user cannot sign up with existing info but can log in.
    #178205565 Alerts a user weather or not information for loging in was correct.

--- login.php
+++ login.php
@@ -11,22 +11,71 @@
 	<?php
 	require('header.php');
   require('functions.php');
+  require('dbConnect.php');
 
-  $submitPressed = sanitizeString(INPUT_POST, 'clickIt');
+  $errorMsg = "Issue with database.";
 
-  if (isset($submitPressed)) {
+  $submitPressedLogIn = sanitizeString(INPUT_POST, 'logIn');
+  $submitPressedSignUp = sanitizeString(INPUT_POST, 'signUp');
+  $user = trim(sanitizeString(INPUT_POST, 'userName'));
+  $passWord = trim(sanitizeString(INPUT_POST, 'passWord'));
 
-    $user = sanitizeString(INPUT_POST, 'userName');
-    $passWord = sanitizeString(INPUT_POST, 'passWord');
+  $greeting = "Please log in or sign up for deliciousness";
+
+  if (isset($submitPressedLogIn)) {// Form was submitted.
+
+    $userResult = callquery($pdo, 'SELECT * FROM `login`', 
+                            'Error fetching users');
 
     if ($user != "" && $passWord != "") {
-      $greeting = "Welcome to Desk Dash $user";
+      $query = "SELECT COUNT(username)
+      FROM `login`
+      WHERE `username` = '$user' AND `password` = '$passWord'";
+
+
+
+      $numUsers = callQuery($pdo, $query, $errorMsg)->fetchColumn();
+
+      if ($numUsers) {
+        $greeting = "Welcome back to desk dash $user!";
+      } else {
+        $greeting = "This user could not be found, please check credentials or sign up.";
+      }
+      
     } else {
       $greeting = "Please enter a username and password.";
     }
-  } else {
-    $greeting = "Please login for deliciousness!";
-  }
+
+  } 
+  if (isset($submitPressedSignUp)) {
+
+    if ($user != "" && $passWord != "") {
+    
+      $query = "SELECT COUNT(username)
+      FROM `login`
+      WHERE username = '$user'";
+
+      $numUsers = callQuery($pdo, $query, $errorMsg)->fetchColumn();
+
+        if (!$numUsers) {
+
+          $query = "INSERT INTO `login` (`username`, `password`) VALUES ('$user', '$passWord')";
+
+          callQuery($pdo, $query, $errorMsg);
+
+          $greeting = "Welcome new user $user we hope you enjoy!";
+
+        } else {
+          $greeting = "This user already exists, try a different username.";
+        }
+
+    } else {
+      $greeting = "Please enter a username and password.";
+    }
+    
+  } 
+    
+  
 
 	?>
   <div id="content">
@@ -43,7 +92,8 @@
         <input type="password" placeholder="Password" name="passWord" id="passWord">
         <br><br>
               
-        <input type="submit" name="clickIt" value="Log In" id="submit">
+        <input type="submit" name="logIn" value="Log In" id="submit">
+        <input type="submit" name="signUp" value="Sign Up" id="submit">
       </form>
   </div>
 	<?php
